95f6c22c06ae7f02053adc5e2d11293710671fe0
Morkovka21Vek <142694961+Morkovka21Vek@users.noreply.github.com>
2024-06-30 21:22:16 +0300

    Fix Bug

--- util/createFolders.py
+++ util/createFolders.py
@@ -8,9 +8,10 @@ if not os.path.exists(os.path.join(directory, 'models')):
 if not os.path.exists(os.path.join(directory, 'output', 'svg')):
     os.makedirs(os.path.join(directory, 'output', 'svg'))
     
-if True:
-    shutil.rmtree(os.path.join(directory, 'cache'))
-    
+if os.path.exists(os.path.join(directory, 'cache')):
+    if True:
+        shutil.rmtree(os.path.join(directory, 'cache'))
+        
 if not os.path.exists(os.path.join(directory, 'cache')):
     os.makedirs(os.path.join(directory, 'cache'))
 
